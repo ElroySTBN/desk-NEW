429fa0798dd954c21b16068434f378fef827c14f
Elroy Sitbon <elroystbn@gmail.com>
2025-11-01 12:11:00 +0100

    Refactor: Nettoyage fichiers obsolètes

--- src/integrations/supabase/types_new.ts
+++
@@ -1,2 +0,0 @@
-npm warn exec The following package was not found and will be installed: supabase@2.54.11
-2025/11/01 12:09:39 Access token not provided. Supply an access token by running supabase login or setting the SUPABASE_ACCESS_TOKEN environment variable.
